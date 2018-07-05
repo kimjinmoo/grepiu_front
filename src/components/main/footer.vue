acc7b04c19fd6a857ad3986849c8d9c871b9850b
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-07-05 17:18:43 +0900

    1. 하단 fix

--- src/components/main/footer.vue
+++ src/components/main/footer.vue
@@ -1,16 +1,8 @@
 <template>
-  <footer>
-    <ul style="list-style-type: none;">
-      <li>
-        <a href="https://github.com/kimjinmoo" target="_blank"><i class="fa custom_fa fa-github" ></i></a>
-      </li>
-      <li>
-        <p class="m-0 text-center light">이메일 : iukim21c@gmail.com</p>
-      </li>
-      <li>
-        <p class="m-0 text-center light">대한민국 서울 / Since 2018, GrepIU</p>
-      </li>
-    </ul>
+  <footer class="footer_style">
+    <a href="https://github.com/kimjinmoo" target="_blank"><i class="fa custom_fa fa-github" ></i></a>
+    <p class="text-center light">이메일 : iukim21c@gmail.com</p>
+    <p class="text-center light">대한민국 서울 / Since 2018, GrepIU</p>
   </footer>
 </template>
 <script>
@@ -19,12 +11,17 @@
   }
 </script>
 <style>
+  .footer_style {
+    width: 100%;
+    height : 74px;
+    text-align: center;
+  }
   .custom_fa {
-    padding: 20px;
-    font-size: 30px;
+    padding: 6px;
+    font-size: 24px;
     width: 50px;
     text-align: center;
     text-decoration: none;
-    color: #0d95e8;
+    color: black;
   }
 </style>
--- src/components/member/signIn.vue
+++ src/components/member/signIn.vue
@@ -1,24 +1,21 @@
 <template>
-  <div class="container-fluid">
-    <div class="container text-center">
-      <form class="form-signin" data-ember-action="2">
-        <h2 class="form-signin-heading">로그인</h2>
-        <small class="text-muted">Login via</small>
-        <br><br>
-        <p>
-          <a class="btn btn-primary fa fa-google" href="javascript:void(0);" v-on:click="signInByGoogle"><i class="fa icon-social-google"></i></a>
-          &nbsp;
-          <a class="btn btn-primary fa fa-facebook" href="javascript:void(0);" v-on:click="signInByFaceBook"><i class="fa icon-social-facebook"></i></a>
-        </p>
-        <small class="text-muted">with firebase</small>
-        <!--<input id="email" autocomplete="off"  v-model="form.email" class="ember-view ember-text-field form-control login-input" placeholder="이메일주소" type="text">-->
-        <!--<input id="password" autocomplete="off" v-model="form.password" class="ember-view ember-text-field form-control login-input-pass" placeholder="비밀번호" type="password">-->
+  <div class="center">
+    <h1>#Login</h1>
+    <form class="form-signin" data-ember-action="2">
+      <p class="small m-0">아래 로그인을 지원합니다.</p>
+      <p class="small">버튼을 클릭 하세요.</p>
+      <p>
+        <a class="btn btn-primary fa fa-google" href="javascript:void(0);" v-on:click="signInByGoogle"><i class="fa icon-social-google"></i></a>
+        &nbsp;
+        <a class="btn btn-primary fa fa-facebook" href="javascript:void(0);" v-on:click="signInByFaceBook"><i class="fa icon-social-facebook"></i></a>
+      </p><small class="text-muted">with Google Firebase</small>
+      <!--<input id="email" autocomplete="off"  v-model="form.email" class="ember-view ember-text-field form-control login-input" placeholder="이메일주소" type="text">-->
+      <!--<input id="password" autocomplete="off" v-model="form.password" class="ember-view ember-text-field form-control login-input-pass" placeholder="비밀번호" type="password">-->
 
-        <!--<button class="btn btn-lg btn-primary btn-block btn-center" v-on:click="signIn" data-bindattr-3="3">로그인</button>-->
-        <!--<br>-->
-        <!--<small class="create-account text-muted">회원가입을 하지 않으셨습니까 ?<b-button id="ember363" class="ember-view btn btn-sm btn-default" to="/signUp">일반회원가입</b-button></small>-->
-      </form>
-    </div>
+      <!--<button class="btn btn-lg btn-primary btn-block btn-center" v-on:click="signIn" data-bindattr-3="3">로그인</button>-->
+      <!--<br>-->
+      <!--<small class="create-account text-muted">회원가입을 하지 않으셨습니까 ?<b-button id="ember363" class="ember-view btn btn-sm btn-default" to="/signUp">일반회원가입</b-button></small>-->
+    </form>
   </div>
 </template>
 <script>
@@ -99,3 +96,16 @@
     }
   }
 </script>
+<style>
+  main {
+    min-height:80vh;
+  }
+  .center {
+    vertical-align: middle;
+    top: 50%;
+    left: 50%;
+    text-align: center;
+    margin: auto;
+    padding: 5px;
+  }
+</style>
