a068c3e30ce001bd2b693cca8f9ee07822d86179
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-08-28 17:33:59 +0900

    1. Layout 잡는중

--- src/components/main/home.vue
+++ src/components/main/home.vue
@@ -13,9 +13,9 @@
         </b-row>
       </b-container>
     </div>
-    <div class="bg-light">
+    <div style="background-color: #ffc107">
       <div class="container">
-        <b-container class="bg-light">
+        <b-container style="padding-top: 1rem">
           <b-row>
             <b-col sm class="greph">
               <h3>크롤링 테스트</h3>
@@ -23,7 +23,7 @@
               <h6>- 현 위치 기준 근처 영화관 찾기(GPS 필요)</h6>
               <h6>- 상영 영화/상영시간 제공</h6>
               <div class="grepb_m1">
-                <b-button to="/lab/movie">확인하기</b-button>
+                <b-button to="/lab/movie" variant="outline-success" >확인하기</b-button>
               </div>
             </b-col>
             <b-col sm class="greph">
@@ -31,7 +31,7 @@
               <h6>- 개발/일상 관련 내용 정리</h6>
               <h6>- MongoDB로 구현</h6>
               <div class="grepb_m1">
-                <b-button to="/post" target="_blank">확인하기</b-button>
+                <b-button to="/post" variant="outline-success">확인하기</b-button>
               </div>
             </b-col>
             <b-col sm class="greph">
@@ -39,23 +39,49 @@
               <h6>- 스웨거 API 문서 프레임웍 적용</h6>
               <h6>- 스프링 Boot 2.0에 스웨거 적용</h6>
               <h6>- 적용 버전 2.8.2</h6>
-              <b-button href="https://conf.grepiu.com/swagger-ui.html" target="_blank">확인하기
+              <b-button href="https://conf.grepiu.com/swagger-ui.html" target="_blank" variant="outline-success">확인하기
               </b-button>
             </b-col>
           </b-row>
         </b-container>
       </div>
     </div>
-    <div class="container">
+    <div class="container" style="padding-top: 1rem">
       <b-container>
         <b-row>
-          <b-col sm="2" style="margin-top: 1rem">
-            <h3>실험실</h3>
+          <b-col sm="2">
+            <h2>실험실</h2>
           </b-col>
         </b-row>
         <b-row>
+          <b-col sm>
+            <h3>Server Side</h3>
+            <h6>Java</h6>
+            <h6>Python</h6>
+            <h6>Node</h6>
+          </b-col>
+          <b-col sm>
+            <h3>Front</h3>
+            <h6>Vue</h6>
+            <h6>Javascript</h6>
+          </b-col>
+          <b-col sm>
+            <h3>Framework</h3>
+            <h6>SpringBoot Oauth 구축</h6>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col sm>
+            <h3>BigData</h3>
+            <h6>엘라스틱서치</h6>
+          </b-col>
           <b-col sm class="greph">
-            <h6>신비로운 기능</h6>
+            <h3>ETC</h3>
+            <h6>Socket 통신</h6>
+          </b-col>
+          <b-col sm class="greph">
+            <h3>Linux</h3>
+            <h6>명령어 모음</h6>
           </b-col>
         </b-row>
       </b-container>
