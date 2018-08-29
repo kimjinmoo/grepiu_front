e31ab5cfddc2d960fa34d3c27d648e606d3bd9e8
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-08-29 16:31:41 +0900

    1. home 수정

--- src/components/main/home.vue
+++ src/components/main/home.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="container-fluid h-100 ">
-    <div class="container">
+    <div class="container" style="background-image: url('/static/img/home_bg.png');background-repeat: no-repeat; background-size: auto">
       <b-container>
         <b-row>
           <b-col sm class="greph">
-            <h1>재미있게 개발하자</h1>
-            <h4 class="m-lg-2">아자아자!!</h4>
-          </b-col>
-          <b-col sm class="greph">
-            Q
+            <h1>즐기는 개발자</h1>
+            <h5 class="m-lg-2">잼나게 개발하자!!</h5>
           </b-col>
         </b-row>
       </b-container>
@@ -18,7 +15,7 @@
         <b-container style="padding-top: 1rem">
           <b-row>
             <b-col sm class="greph">
-              <h3>크롤링 테스트</h3>
+              <h3>크롤링</h3>
               <h6>- Selenium을 통한 롯데시네마 데이터 크롤링</h6>
               <h6>- 현 위치 기준 근처 영화관 찾기(GPS 필요)</h6>
               <h6>- 상영 영화/상영시간 제공</h6>
@@ -27,18 +24,18 @@
               </div>
             </b-col>
             <b-col sm class="greph">
-              <h3>포스트</h3>
-              <h6>- 개발/일상 관련 내용 정리</h6>
-              <h6>- MongoDB로 구현</h6>
+              <h3>게시판</h3>
+              <h6>- 개발/일상 관련 작성 예정</h6>
+              <h6>- MongoDB 및 Spring Data MongoDB 를 이용해 구현</h6>
               <div class="grepb_m1">
                 <b-button to="/post" variant="outline-success">확인하기</b-button>
               </div>
             </b-col>
             <b-col sm class="greph">
-              <h3>API 문서 자동화</h3>
-              <h6>- 스웨거 API 문서 프레임웍 적용</h6>
-              <h6>- 스프링 Boot 2.0에 스웨거 적용</h6>
-              <h6>- 적용 버전 2.8.2</h6>
+              <h3>배포</h3>
+              <h6>- Docker를 통한 Jenkins 세팅</h6>
+              <h6>- gitlab 연동 및 브런치별 빌드</h6>
+              <h6>- 슬랙을 통한 빌드</h6>
               <b-button href="https://conf.grepiu.com/swagger-ui.html" target="_blank" variant="outline-success">확인하기
               </b-button>
             </b-col>
@@ -55,19 +52,20 @@
         </b-row>
         <b-row>
           <b-col sm>
-            <h3>Server Side</h3>
+            <h3>Server</h3>
             <h6>Java</h6>
             <h6>Python</h6>
             <h6>Node</h6>
           </b-col>
           <b-col sm>
-            <h3>Front</h3>
+            <h3>Client</h3>
             <h6>Vue</h6>
             <h6>Javascript</h6>
+            <h6>freemarker</h6>
           </b-col>
           <b-col sm>
-            <h3>Framework</h3>
-            <h6>SpringBoot Oauth 구축</h6>
+            <h3>Login</h3>
+            <h6>SpringBoot Oauth</h6>
           </b-col>
         </b-row>
         <b-row>
@@ -78,10 +76,11 @@
           <b-col sm class="greph">
             <h3>ETC</h3>
             <h6>Socket 통신</h6>
+            <h6>Android 개발</h6>
           </b-col>
           <b-col sm class="greph">
             <h3>Linux</h3>
-            <h6>명령어 모음</h6>
+            <h6>centos</h6>
           </b-col>
         </b-row>
       </b-container>
