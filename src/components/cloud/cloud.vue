0ec9da525740eabe443dbf24c8b27c0011839f94
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-11-09 17:51:08 +0900

    1. 클라우드 스토리지 추가

--- src/components/cloud/cloud.vue
+++ src/components/cloud/cloud.vue
@@ -1,19 +1,57 @@
 <template>
   <div class="container-fluid">
-      <h1>Cloud</h1>
-      <div style="height: 50vh;" class="border border-danger bg-light">
-
-      </div>
+    <h2>Cloud</h2>
+    <a href="#" @click="createNewFolder">폴더 생성</a>
+    <div style="height: 50vh;" class="border border-danger bg-light">
+      <ul v-for="item in dir" style="float: left;list-style:none;padding:5px;">
+        <li v-if="item.attribute == 'P'" style="float:left; margin-top: 5px" @click="clickFolder">
+          <img src="/static/img/cloud/folder.png" style="width: 64px; height: auto;">
+          <div style="text-align: center">{{item.name}}</div>
+        </li>
+        <li v-else style="float:left; margin-top: 5px" @click="clickFolder">
+          <img src="/static/img/cloud/file.png" style="width: 64px; height: auto;">
+          <div style="text-align: center">{{item.name}}</div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
   export default {
     name: "cloud",
+    data : function(){
+      return {
+        dir: []
+      }
+    },
     created() {
-
+      this.getFolder();
     },
     methods: {
-
+      getFolder : function() {
+        this.$http.get(process.env.ROOT_API + "/grepiu/cloud/", {
+          params: {
+            id: 'iukim21c@gmail.com',
+            path: "/"
+          }
+        }).then((response) => {
+            this.dir = response.data;
+        })
+      },
+      createNewFolder: function() {
+        this.$http.post(process.env.ROOT_API + "/grepiu/cloud/",
+          {
+            attribute: 'P',
+            createId: 'iukim21c@gmail.com',
+            name: '새폴더',
+            path: "/"
+          }).then((response) => {
+          this.getFolder();
+        })
+      },
+      clickFolder: function(obj) {
+        console.log(obj);
+      }
     }
   }
 </script>
