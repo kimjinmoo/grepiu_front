549242126de67028e7e12a20a8dfa0a60a3496f9
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2018-11-19 16:08:47 +0900

    1. 클라우드 스토리지 추가

--- src/components/cloud/cloud.vue
+++ src/components/cloud/cloud.vue
@@ -2,58 +2,82 @@
   <div class="container-fluid">
     <h2>Cloud</h2>
     <div>경로 : {{currentDir}}</div>
-    <a href="#" @click="createNewFolder">폴더 생성</a>
-    <a href="#" @click="movePre">위로</a>
+    <b-button-group size="sm">
+      <b-button variant="success" @click="createNewFolder">폴더 생성</b-button>
+      <b-button variant="success" @click="moveTop">최상위경로</b-button>
+    </b-button-group>
     <div style="height: 50vh;" class="border border-danger bg-light">
-      <ul v-for="item in dir" style="float: left;list-style:none;padding:5px;">
-        <li v-if="item.attribute == 'P'" style="float:left; margin-top: 5px" @click="read(item.attribute)">
-          <img src="/static/img/cloud/folder.png" style="width: 64px; height: auto;">
-          <div style="text-align: center">{{item.name}}</div>
+      <ul v-for="item in folders" style="float: left;list-style:none;padding:5px;">
+        <li v-if="item.attribute == 'P'" style="float:left; margin-top: 5px">
+          <img src="/static/img/cloud/folder.png" style="width: 64px; height: auto;" @click="read(item.id, item.attribute, item.name)">
+          <div style="text-align: center"><label>{{item.name}}</label></div>
+          <!--<input type="text" style="text-align: center" v-model="item.name" @blur="updateFolder(item.id, item.name)"></input>-->
         </li>
         <li v-else style="float:left; margin-top: 5px" @click="read(item.attribute)">
           <img src="/static/img/cloud/file.png" style="width: 64px; height: auto;">
-          <div style="text-align: center">{{item.name}}</div>
+          <div style="text-align: center">
+            <lable >{{item.name}}</lable>
+          </div>
         </li>
       </ul>
     </div>
   </div>
 </template>
 <script>
+  import Lab from "../lab/lab";
   export default {
     name: "cloud",
+    components: {Lab},
     data : function(){
       return {
-        preDir : "/",
-        currentDir : "/",
-        dir: []
+        obj: [],
+        pid: "",
+        preDir: "",
+        currentDir: "/",
+        folders: []
       }
     },
     created() {
       this.getFolder();
     },
+    computed: {
+      upDir : function() {
+        let folders = [];
+        let pos = 0, end=0;
+
+        while ((end = this.currentDir.indexOf("/", pos)) >= 0) {
+          folders.push(this.currentDir.substring(pos, end));
+          pos= end+1;
+        }
+        folders.pop();
+        return folders.join("/")+"/";
+      }
+    },
     methods: {
+      moveTop: function() {
+        this.pid = "";
+        this.currentDir = "/";
+        this.getFolder();
+      },
       movePre : function() {
-        this.currentDir = this.preDir;
+        this.currentDir = this.upDir();
         this.getFolder();
       },
       getFolder : function() {
         this.$http.get(process.env.ROOT_API + "/grepiu/cloud/", {
           params: {
-            id: 'iukim21c@gmail.com',
-            path: this.currentDir
+            pid: this.pid
           }
         }).then((response) => {
-          console.log(response.data);
-          this.dir = response.data;
+          this.folders = response.data;
         })
       },
-      read : function(type, name) {
-        console.log(type);
+      read : function(pid, type, name) {
+        this.pid = pid;
         switch (type) {
           case "P" :
-            this.preDir = this.currentDir;
-            this.currentDir+="name"
-            this.getFolder()
+            this.currentDir+=name+"/";
+            this.getFolder();
             break;
           case "F" :
             break;
@@ -65,8 +89,20 @@
             attribute: 'P',
             createId: 'iukim21c@gmail.com',
             name: '새폴더',
+            pid: this.pid,
             path: this.currentDir
           }).then((response) => {
+          this.getFolder();
+        })
+      },
+      updateFolder: function(fid, name) {
+        console.log("들어옴");
+        const querystring = require('querystring');
+
+        this.$http.put(process.env.ROOT_API + "/grepiu/cloud/",{
+          id: fid,
+          name: name
+        }).then(v=>{
           this.getFolder();
         })
       }
