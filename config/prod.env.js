837d953ac6a24d8461ed9f86a3be1f89071cd5a9
김진무(iukim21c@smartlo.net) <iukim21c@gmail.com>
2019-01-14 15:55:58 +0900

    1. PROD_GOOGLE_KEY

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   ROOT_API : '"https://conf.grepiu.com"',
-  DATA_API : '"https://data.grepiu.com"'
+  DATA_API : '"https://data.grepiu.com"',
+  GOOGLE_API_KEY : '"$GOOGLE_API_KEY$"'
 }
